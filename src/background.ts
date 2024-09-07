ba045316f310a2ff547c39cb5d55d0ce444d7c6c
Chris LaRose <cjlarose@gmail.com>
2024-09-06 19:11:37 -0700

    Remove unused code for handling connections from content script

--- src/background.ts
+++ src/background.ts
@@ -5,43 +5,12 @@ import {
   handlePacket,
 } from "./lib/connection";
 
-async function getSubscibedTabs(): Promise<Set<number>> {
-  const { tabs } = await chrome.storage.local.get("tabs");
-  if (tabs === undefined) {
-    return new Set();
-  }
-  return new Set(tabs);
-}
-
-async function addTabSubscription(tabId: number) {
-  const { tabs } = await chrome.storage.local.get("tabs");
-  const tabSet = new Set(tabs);
-  tabSet.add(tabId);
-  return chrome.storage.local.set({ tabs: [...tabSet] });
-}
-
-async function removeTabSubscription(tabId: number) {
-  const { tabs } = await chrome.storage.local.get("tabs");
-  const tabSet = new Set(tabs);
-  tabSet.delete(tabId);
-  return chrome.storage.local.set({ tabs: [...tabSet] });
-}
-
 async function getConnectionState(): Promise<ConnectionState> {
   const { connectionState } = await chrome.storage.local.get("connectionState");
   return connectionState;
 }
 
 async function sendUpdatedStateToSubscribers(state: ConnectionState) {
-  const tabs = await getSubscibedTabs();
-  for (const tabId of tabs) {
-    try {
-      await chrome.tabs.sendMessage(tabId, state);
-    } catch (e) {
-      await removeTabSubscription(tabId);
-    }
-  }
-
   for (const port of activePorts) {
     port.postMessage(state);
   }
@@ -89,7 +58,6 @@ chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
   if (!tab.url) return;
   const url = new URL(tab.url);
   if (url.origin === GG_ORIGIN && url.pathname === GG_PATH) {
-    await addTabSubscription(tabId);
     await chrome.sidePanel.setOptions({
       tabId,
       path: "side-panel.html",
