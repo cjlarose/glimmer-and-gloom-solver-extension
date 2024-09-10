ea8c8b5291830c526b6af434a67b59f2469401cd
Chris LaRose <cjlarose@gmail.com>
2024-09-09 18:43:40 -0700

    Extract storage key into constant

--- src/lib/connection_state_storage.ts
+++ src/lib/connection_state_storage.ts
@@ -1,21 +1,23 @@
 import { ConnectionState, initialState } from "./connection_state";
 
+const STORAGE_KEY = "connectionState";
+
 export async function getConnectionState(): Promise<ConnectionState> {
-  const { connectionState } = await chrome.storage.local.get("connectionState");
+  const { connectionState } = await chrome.storage.local.get(STORAGE_KEY);
   return connectionState === undefined ? initialState : connectionState;
 }
 
 export async function setConnectionState(
   state: ConnectionState,
 ): Promise<void> {
-  await chrome.storage.local.set({ connectionState: state });
+  await chrome.storage.local.set({ [STORAGE_KEY]: state });
 }
 
 export function addStateChangeListener(
   callback: (connectionState: ConnectionState) => void,
 ): void {
   chrome.storage.local.onChanged.addListener((changes) => {
-    const stateChange = changes["connectionState"];
+    const stateChange = changes[STORAGE_KEY];
 
     if (!stateChange) {
       return;
