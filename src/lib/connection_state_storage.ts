56d1f82138f27960fad54d172ad078587ce8b435
Chris LaRose <cjlarose@gmail.com>
2024-09-09 18:45:39 -0700

    Store connection state in session storage

--- src/lib/connection_state_storage.ts
+++ src/lib/connection_state_storage.ts
@@ -3,20 +3,20 @@ import { ConnectionState, initialState } from "./connection_state";
 const STORAGE_KEY = "connectionState";
 
 export async function getConnectionState(): Promise<ConnectionState> {
-  const { connectionState } = await chrome.storage.local.get(STORAGE_KEY);
+  const { connectionState } = await chrome.storage.session.get(STORAGE_KEY);
   return connectionState === undefined ? initialState : connectionState;
 }
 
 export async function setConnectionState(
   state: ConnectionState,
 ): Promise<void> {
-  await chrome.storage.local.set({ [STORAGE_KEY]: state });
+  await chrome.storage.session.set({ [STORAGE_KEY]: state });
 }
 
 export function addStateChangeListener(
   callback: (connectionState: ConnectionState) => void,
 ): void {
-  chrome.storage.local.onChanged.addListener((changes) => {
+  chrome.storage.session.onChanged.addListener((changes) => {
     const stateChange = changes[STORAGE_KEY];
 
     if (!stateChange) {
